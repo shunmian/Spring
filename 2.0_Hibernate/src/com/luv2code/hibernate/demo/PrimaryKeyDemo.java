2cb0c5dbd97e409597954833e0040234f3467895
shunmian <shunmian@gmail.com>
2018-09-16 12:48:48 +0800

    FEAT(Hibernate): use CRUD via Hibernates

---
+++ 2.0_Hibernate/src/com/luv2code/hibernate/demo/PrimaryKeyDemo.java
@@ -0,0 +1,53 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class PrimaryKeyDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// use the session object to save Java object
+			System.out.println("Creating new student object...");
+			Student tempStudent1 = new Student("Paul1",  "Wall", "paul@luv2code.com");
+			Student tempStudent2 = new Student("Paul2",  "Wall", "paul@luv2code.com");
+			Student tempStudent3 = new Student("Paul3",  "Wall", "paul@luv2code.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student...");
+			session.save(tempStudent1);
+			session.save(tempStudent2);
+			session.save(tempStudent3);
+			
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+			
+		} catch (Exception e) {
+			
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
---
+++ 2.0_Hibernate/src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// use the session object to save Java object
+			System.out.println("Creating new student object...");
+//			Student tempStudent = new Student("Paul",  "Wall", "paul@luv2code.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			System.out.println("Saving the student...");
+			Student myStudent = session.get(Student.class, 4);
+			
+			List<Student> theStudents = session.createQuery("from Student").list();
+			
+			for (Student tempStudent : theStudents) {
+				System.out.println(tempStudent);
+			}
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!" + myStudent);
+			
+			
+		} catch (Exception e) {
+			
+		} finally {
+			factory.close();
+		}
+		
+	}
+
+}
